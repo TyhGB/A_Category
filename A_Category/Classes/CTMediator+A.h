cefaef13b42f11ec1de211d75a25a0d0dfcffc8e
TyhGB <30275349+TyhGB@users.noreply.github.com>
2019-07-18 16:14:24 +0800

    fix

--- A_Category/Classes/CTMediator+A.h
+++ A_Category/Classes/CTMediator+A.h
@@ -11,7 +11,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface CTMediator (A)
-
+- (UIViewController *)A_aViewController;
 @end
 
 NS_ASSUME_NONNULL_END
--- A_Category/Classes/CTMediator+A.m
+++ A_Category/Classes/CTMediator+A.m
@@ -9,5 +9,11 @@
 #import "CTMediator+A.h"
 
 @implementation CTMediator (A)
-
+- (UIViewController *)A_aViewController
+{
+    /*
+     AViewController *viewController = [[AViewController alloc] init];
+     */
+    return [self performTarget:@"A" action:@"viewController" params:nil shouldCacheTarget:NO];
+}
 @end
