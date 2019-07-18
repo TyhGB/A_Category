fc4be6d6b53561c042958c3dd501bc4492518dc5
TyhGB <30275349+TyhGB@users.noreply.github.com>
2019-07-18 14:55:23 +0800

    2

---
+++ A_Category/Classes/CTMediator+A.h
@@ -0,0 +1,17 @@
+//
+//  CTMediator+A.h
+//  A_Category
+//
+//  Created by TyhOS on 2019/7/18.
+//  Copyright © 2019 上海佰量网络科技. All rights reserved.
+//
+
+#import "CTMediator.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CTMediator (A)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ A_Category/Classes/CTMediator+A.m
@@ -0,0 +1,13 @@
+//
+//  CTMediator+A.m
+//  A_Category
+//
+//  Created by TyhOS on 2019/7/18.
+//  Copyright © 2019 上海佰量网络科技. All rights reserved.
+//
+
+#import "CTMediator+A.h"
+
+@implementation CTMediator (A)
+
+@end
